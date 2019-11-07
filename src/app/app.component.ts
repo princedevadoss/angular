6555cd71891bd748adee44635da4caf774f2137f
Devadoss <prince.devadoss@sap.com>
2019-11-07 23:16:57 +0530

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angul';
+  menu = [ 'CONTACT US', 'MACHINERY', 'ABOUT US', '   HOME'];
 }
